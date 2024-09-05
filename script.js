42c359a1cfd3f5a6cbc877e9262cf04022ed05d6
NicoDev <167468234+N1colasT0rres@users.noreply.github.com>
2024-09-04 20:51:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Puedes añadir funciones para mejorar la interactividad aquí
+
+// Ejemplo: mostrar un mensaje de bienvenida cuando se carga la página
+window.onload = function() {
+    console.log("Bienvenido a LuxuryShop");
+};
